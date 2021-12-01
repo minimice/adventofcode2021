b464493c9f0373bd6fc80d4a9392456c9536ca88
Chooi-Guan Lim <cholim@on.sinch.com>
2021-12-02 00:23:30 +0100

    Solve day 1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 /* Author: Lim, Chooi Guan */
 /* Find me at https://linkedin.com/in/cgl88 */
@@ -9,7 +10,26 @@ namespace day1
 	{
 		public static void Main(string[] args)
 		{
+			/* Part 1 */
 			var lines = System.IO.File.ReadAllLines(@"../../input.txt");
+			Count(lines);
+
+			/* Part 2 */
+			lines = System.IO.File.ReadAllLines(@"../../input.txt");
+			var newSums = new List<string>();
+			/* Construct new sums*/
+			for (var i = 0; i < lines.Length - 2; i++)
+			{
+				var num1 = Int32.Parse(lines[i]);
+				var num2 = Int32.Parse(lines[i + 1]);
+				var num3 = Int32.Parse(lines[i + 2]);
+				newSums.Add((num1 + num2 + num3).ToString());
+			}
+			Count(newSums.ToArray());
+		}
+
+		public static void Count(string[] lines)
+		{
 			var previous = Int32.Parse(lines[0]);
 			var count = 0;
 			for (var i = 1; i < lines.Length; i++)
