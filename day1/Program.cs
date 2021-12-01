22c264f27deac068ebd359c40301764494d3d99e
Chooi-Guan Lim <cholim@on.sinch.com>
2021-12-02 00:02:50 +0100

    Solved day 1

---
+++ day1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/* Author: Lim, Chooi Guan */
+/* Find me at https://linkedin.com/in/cgl88 */
+
+namespace day1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var lines = System.IO.File.ReadAllLines(@"../../input.txt");
+			var previous = Int32.Parse(lines[0]);
+			var count = 0;
+			for (var i = 1; i < lines.Length; i++)
+			{
+				var current = Int32.Parse(lines[i]);
+				var increased = current > previous;
+				if (increased)
+				{
+					count++;
+				}
+				previous = current;
+			}
+			Console.WriteLine(count);
+		}
+	}
+}
